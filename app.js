8f90c8c7848c512d92704dec731773a0fea4d6cf
GabrielaGuzmanR <gguzmanr@unal.edu.co>
2025-04-06 13:28:47 -0500

    allow cors

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: '*'
 }));
 
 app.use(express.json());
